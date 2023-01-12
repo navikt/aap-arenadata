38875b504d01d387db2b803dbdda7943cb5e1a23
Richard Borge <richard.borge@nav.no>
2023-01-12 10:28:42 +0100

    Legger til en test for å få testregimet til å funke.
    Co-authored-by: Niro Karunakaran <niro.karunakaran@nav.no>

---
+++ app/src/test/kotlin/no/nav/aap/AppTest.kt
@@ -0,0 +1,12 @@
+package no.nav.aap
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class AppTest {
+
+    @Test
+    fun hello() {
+        assertTrue(true)
+    }
+}
