45ad4dfc7e2495b241e77228cd34eccdb1805b23
VHollund <vetlehoo@gmail.com>
2023-10-06 10:08:34 +0200

    lagt til call logging, og statuspages, og fikset env vars
    Co-authored-by: Richard Borge <richard.borge@nav.no>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,6 +23,8 @@ dependencies {
     implementation("io.ktor:ktor-server-auth:$ktorVersion")
     implementation("io.ktor:ktor-server-auth-jwt:$ktorVersion")
     implementation("io.ktor:ktor-server-metrics-micrometer:$ktorVersion")
+    implementation("io.ktor:ktor-server-call-logging:$ktorVersion")
+    implementation("io.ktor:ktor-server-status-pages:$ktorVersion")
 
     implementation("io.ktor:ktor-client-jackson:$ktorVersion")
     implementation("io.ktor:ktor-client-cio:$ktorVersion")
--- app/main/saksinfo/App.kt
+++ app/main/saksinfo/App.kt
@@ -3,6 +3,7 @@ package saksinfo
 import com.auth0.jwk.JwkProviderBuilder
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import io.ktor.http.*
 import io.ktor.serialization.jackson.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
@@ -11,7 +12,9 @@ import io.ktor.server.engine.*
 import io.ktor.server.metrics.micrometer.*
 import io.ktor.server.netty.*
 import io.ktor.server.plugins.*
+import io.ktor.server.plugins.callloging.*
 import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -21,15 +24,17 @@ import no.nav.aap.kafka.streams.v2.Streams
 import no.nav.aap.kafka.streams.v2.KafkaStreams
 import no.nav.aap.ktor.config.loadConfig
 import org.slf4j.LoggerFactory
+import org.slf4j.event.Level
 import saksinfo.arena.ArenaoppslagRestClient
 import saksinfo.kafka.Tables
 import saksinfo.kafka.topology
-import java.time.LocalDate
 import java.util.concurrent.TimeUnit
 
 private val sikkerLogg = LoggerFactory.getLogger("secureLog")
+private val logger = LoggerFactory.getLogger("App")
 
 fun main() {
+    Thread.currentThread().setUncaughtExceptionHandler { _, e -> sikkerLogg.error("Uhåndtert feil", e) }
     embeddedServer(Netty, port = 8080, module = Application::server).start(wait = true)
 }
 
@@ -45,7 +50,25 @@ private fun Application.server(kafka: Streams = KafkaStreams()) {
         }
     }
 
-    Thread.currentThread().setUncaughtExceptionHandler { _, e -> sikkerLogg.error("Uhåndtert feil", e) }
+    install(CallLogging) {
+        level = Level.INFO
+        format { call ->
+            val status = call.response.status()
+            val httpMethod = call.request.httpMethod.value
+            val userAgent = call.request.headers["User-Agent"]
+            val callId = call.request.header("x-callId") ?: call.request.header("nav-callId") ?: "ukjent"
+            "Status: $status, HTTP method: $httpMethod, User agent: $userAgent, callId: $callId"
+        }
+        filter { call -> call.request.path().startsWith("/actuator").not() }
+    }
+
+    install(StatusPages) {
+        exception<Throwable> { call, cause ->
+            logger.error("Uhåndtert feil", cause)
+            call.respondText(text = "Feil i tjeneste: ${cause.message}" , status = HttpStatusCode.InternalServerError)
+        }
+    }
+
     environment.monitor.subscribe(ApplicationStopping) { kafka.close() }
 
     val jwkProvider = JwkProviderBuilder(config.oauth.azure.jwksUrl)
